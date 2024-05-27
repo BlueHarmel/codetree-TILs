15f30146dcd9282b6d3585ca308b6ef8b1936856
Young Seok Lee <dldjdtjr@naver.com>
2024-05-27 17:24:11 +0900

    Title: 정수 명령 처리 5, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240527/정수 명령 처리 5/process-numeric-commands-5.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Integer> arrayList = new ArrayList<>();
+
+        for(int i = 0; i < 9; i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+            String order = stk.nextToken();
+            
+            switch(order) {
+                case "push_back":
+                    arrayList.add(Integer.parseInt(stk.nextToken()));
+                    break;
+                case "pop_back":
+                    arrayList.remove(arrayList.size()-1);
+                    break;
+                case "size":
+                    System.out.println(arrayList.size());
+                    break;
+                case "get":
+                    System.out.println(arrayList.get(Integer.parseInt(stk.nextToken())-1));
+                    break;
+            }
+        }
+    }
+}
