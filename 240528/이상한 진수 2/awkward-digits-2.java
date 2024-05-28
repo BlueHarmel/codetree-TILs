a62dec590926cfc8f0218e938193d167f0df026c
Young Seok Lee <dldjdtjr@naver.com>
2024-05-28 23:29:39 +0900

    Title: 이상한 진수 2, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240528/이상한 진수 2/awkward-digits-2.java
+++ 240528/이상한 진수 2/awkward-digits-2.java
@@ -6,13 +6,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String binaryNum = br.readLine();
 
+        int changeCnt = 0;
         for(int i=0; i<binaryNum.length();i++) {
             if (binaryNum.charAt(i)=='0') {
                 StringBuilder sb = new StringBuilder(binaryNum);
                 sb.setCharAt(i,'1');
                 binaryNum = sb.toString();
+                changeCnt++;
                 break;
             }
+        }
+        if (changeCnt == 0) {
+            StringBuilder sb = new StringBuilder(binaryNum);
+            sb.setCharAt(binaryNum.length()-1,'0');
+            binaryNum = sb.toString();
         }
 
         int answer = 0;
