7e0ec360eac2014a04d4cb63d95e1f7a17e50177
Young Seok Lee <dldjdtjr@naver.com>
2024-06-04 00:40:20 +0900

    Title: 최고의 13위치 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240604/최고의 13위치 2/best-place-of-13-2.java
+++ 240604/최고의 13위치 2/best-place-of-13-2.java
@@ -28,18 +28,17 @@ public class Main {
             List<Integer> coinGridRow = coinGrid.get(i);
             for (int j =0; j<(n-2); j++) {
                 sumVal1 = coinGridRow.get(j)+coinGridRow.get(j+1)+coinGridRow.get(j+2);
-                for (int k =(j+5>=n && i+1<n)?++i:i;k<n;k++){
+                for (int k =(j+5>=n && i+1<n)?i+1:i;k<n;k++){
                     coinGridRow = coinGrid.get(k);
                     for(int l =(j+5>=n && i+1<n)?0:j+3;l<n-2;l++){
                         sumVal2 = coinGridRow.get(l)+coinGridRow.get(l+1)+coinGridRow.get(l+2);
-                    }
-                    if ((sumVal1+sumVal2)>maxSumVal) {
-                        maxSumVal=sumVal1+sumVal2;
+                        if ((sumVal1+sumVal2)>maxSumVal) {
+                            maxSumVal=sumVal1+sumVal2;
+                        }
                     }
                 }
             }
         }
-
         System.out.println(maxSumVal);
     }
 }
