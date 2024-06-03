07c062a2cc3d4f501fe68fe9796a626a4d7fccf6
Young Seok Lee <dldjdtjr@naver.com>
2024-06-04 00:26:32 +0900

    Title: 최고의 13위치 2, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240604/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> coinGrid = new ArrayList<>();
+        for (int i =0; i<n;i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine()," ");
+            List<Integer> coinGridRow = new ArrayList<Integer>();
+
+            for (int j =0; j<n; j++) {
+                coinGridRow.add(Integer.parseInt(stk.nextToken()));
+            }
+            coinGrid.add(coinGridRow);
+        }
+        /*
+        회전이 불가능 하므로 첫번째 격자의 시작점을 기준으로 잡고
+        그리드 내의 동전 갯수를 새 이중리스트에 저장
+        */
+        int sumVal1 = 0;
+        int sumVal2 = 0;
+        int maxSumVal = Integer.MIN_VALUE;
+        for (int i = 0; i<n; i++){
+            List<Integer> coinGridRow = coinGrid.get(i);
+            for (int j =0; j<(n-2); j++) {
+                sumVal1 = coinGridRow.get(j)+coinGridRow.get(j+1)+coinGridRow.get(j+2);
+                for (int k =(j+5>=n && i+1<n)?++i:i;k<n;k++){
+                    coinGridRow = coinGrid.get(k);
+                    for(int l =(j+5>=n && i+1<n)?0:j+3;l<n-2;l++){
+                        sumVal2 = coinGridRow.get(l)+coinGridRow.get(l+1)+coinGridRow.get(l+2);
+                    }
+                    if ((sumVal1+sumVal2)>maxSumVal) {
+                        maxSumVal=sumVal1+sumVal2;
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxSumVal);
+    }
+}
