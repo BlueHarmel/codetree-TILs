b25431a524c3e0fc12f47ad4e09536d4e35cc8b4
Young Seok Lee <dldjdtjr@naver.com>
2024-07-17 23:32:46 +0900

    Title: 주어진 수까지의 곱, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240717/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(stk.nextToken());
+        int b = Integer.parseInt(stk.nextToken());
+        int prod = 1;
+        for (int i = a;i<=b;i++){
+            prod*=i;
+        }
+        System.out.println(prod);
+    }
+}
