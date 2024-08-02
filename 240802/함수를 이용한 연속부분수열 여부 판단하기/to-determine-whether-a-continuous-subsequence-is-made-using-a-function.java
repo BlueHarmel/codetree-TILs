79d505ba740639c98ad112e7faa426c614bf2cfe
Young Seok Lee <dldjdtjr@naver.com>
2024-08-02 11:29:58 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240802/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(stk.nextToken()),n2 = Integer.parseInt(stk.nextToken());
+        int[] arrN1 = new int[n1];
+        int[] arrN2 = new int[n2];
+
+        stk = new StringTokenizer(br.readLine());
+        for(int i =0;i<n1;i++){
+            arrN1[i]=Integer.parseInt(stk.nextToken());
+        }
+        stk = new StringTokenizer(br.readLine());
+        for(int i =0;i<n2;i++){
+            arrN2[i]=Integer.parseInt(stk.nextToken());
+        }
+
+        System.out.println(isSubArr(arrN1,arrN2));
+    }
+
+    public static boolean isSubArr(int[] arr1, int[] arr2){
+        for(int i =0; i<arr1.length-2;i++){
+            int[] subArr = Arrays.copyOfRange(arr1,i,i+3);
+            System.out.println(subArr[0] + " " + subArr[1] + " " + subArr[2]);
+            System.out.println(arr2[0] + " " + arr2[1] + " " + arr2[2]);
+            if(subArr == arr2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
