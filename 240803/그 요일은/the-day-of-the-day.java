adeeab7397d01004c7fbe9097df01877a585aa99
Young Seok Lee <dldjdtjr@naver.com>
2024-08-03 12:34:11 +0900

    Title: 그 요일은, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240803/그 요일은/the-day-of-the-day.java
+++ 240803/그 요일은/the-day-of-the-day.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,16 +11,27 @@ public class Main {
 
         int m1=Integer.parseInt(stk.nextToken()),d1 = Integer.parseInt(stk.nextToken());
         int m2 = Integer.parseInt(stk.nextToken()), d2 = Integer.parseInt(stk.nextToken());
-        String date = br.readLine();
+        String day = br.readLine();
 
-        System.out.println(cntDate(m1,d1,m2,d2));
+        System.out.println(cntDate(m1,d1,m2,d2,day));
     }
 
-    public static int cntDate(int m1, int d1, int m2, int d2){
+    public static int cntDate(int m1, int d1, int m2, int d2,String day){
         int[] dates = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+        HashMap<String, Integer> days = new HashMap<String, Integer>(){
+            {
+                put("Mon",0);
+                put("Tue",1);
+                put("Wed",2);
+                put("Thu",3);
+                put("Fri",4);
+                put("Sat",5);
+                put("Sun",6);
+            }
+        };
+        int totalDate = dates[m1]-d1 + d2 - days.get(day);
 
-        int totalDate = dates[m1]-d1 + d2;
-        for(int i =m1+1;i<m2;i++){
+        for(int i=0;i < m2-m1-1;i++){
             totalDate+=dates[i];
         }
 
