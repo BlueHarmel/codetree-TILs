d70670287ccbfbd372b7c3dd60759c427f3a6792
Young Seok Lee <dldjdtjr@naver.com>
2024-08-06 23:08:55 +0900

    Title: 2개 이상의 알파벳, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240806/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+
+        System.out.println(isDiverse(a)?"Yes":"No");
+    }
+
+    public static boolean isDiverse(String a) {
+        char prevChar=a.charAt(0);
+        for(int i=1;i<a.length();i++){
+            if(a.charAt(i)==prevChar)
+                return false;
+
+        }
+        return true;
+    }
+}
