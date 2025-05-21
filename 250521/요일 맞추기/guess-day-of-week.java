c97bf1202cbf45e6dc0536317cdfd68be83898c6
Young Seok Lee <dldjdtjr@gmail.com>
2025-05-21 12:29:56 +0900

    Title: 요일 맞추기, Time: 118ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250521/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] daysOfMonth = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] dates = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+
+        int totalDays1 = d1;
+        for(int i =1;i<m1;i++){
+            totalDays1 += daysOfMonth[i];
+        }
+
+        int totalDays2 = d2;
+        for(int i =1;i<m2;i++){
+            totalDays2 += daysOfMonth[i];
+        }
+
+        int index = (totalDays2-totalDays1+7)%7;
+
+        System.out.println(dates[index]);
+    }
+}
